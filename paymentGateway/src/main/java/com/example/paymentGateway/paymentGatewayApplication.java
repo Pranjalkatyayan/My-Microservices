ee5b6369ce2e6dc21a4eacedf5848a28fa06cac4
Pranjal <pranjalkatyayan01@gmail.com>
2023-02-07 14:15:52 +0530

    2nd commit

--- paymentGateway/src/main/java/com/example/paymentGateway/paymentGatewayApplication.java
+++ paymentGateway/src/main/java/com/example/paymentGateway/paymentGatewayApplication.java
@@ -2,8 +2,10 @@ package com.example.paymentGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication()
+@SpringBootApplication
+@EnableConfigServer
 public class paymentGatewayApplication {
 
 	public static void main(String[] args) {
